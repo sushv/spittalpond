4b2ba36bd44e828d4fff0e6cf2f1e1233ef4cdc6
Beckett Simmons <beckettsimmons@hotmail.com>
2014-08-05 15:35:59 -0300

    Cleaning things up.
    Simplified a lot of code.
    Added docstrings and more comments.
    The SpittalPond Class uses instance variables now.
    do_login() function only takes password parameter as pub_user is set.
    Changed the ToyModel to a class with spittalpond initiated inside.

--- spittalpond.py
+++ spittalpond.py
@@ -1,45 +1,66 @@
 import requests
 import json
 
-class SpittalPond():
-    base_url = "http://tmrbmub01prd:8000"
-    is_logged_in = False
-    pub_user = "root"
-    cookies = None
-    def do_request(self, url, in_data=None, in_file=None):
+class SpittalPond:
+    """" Python implementation to the Oasis Django API
+    
+    This class provides an easy to use front-end interface 
+    to the Oasis mid-tier Django API. 
+    """
+    def __init__(self, base_url, pub_user):
+        """ Initiating instance."""
+        self.base_url = "http://tmrbmub01prd:8000"
+        self.pub_user = "root"
+        self.is_logged_in = False
+        self.cookies = None
+
+    def do_request(self, url, in_data=None, in_file_dict=None):
+        """ Makes a post request.
+        
+        Also automatically passes self.cookies to the post request.
+        This authenticates each request.
+        """
         url_string=url
-        header={"content-type":"application/x-www-form-urlencoded"}
         response=requests.post(
             url_string,
             data=in_data,
+            files=in_file_dict,
             cookies=self.cookies  #For Authentication!
-        ) #headers=header);
+        )
         return response
+        
+    def do_login(self, password):
+        """ Logs into Oasis Django mid-tier
+        
+        Also, set self.cookies to the return session ID.
+        This allows for future authentication.
+        """
+        # Creating JSON string with authentication credentails.
+        in_data = ('{{ "username":"{username}",'
+                    '"password":"{password}" }}'
+                    ).format(
+                    username=self.pub_user, 
+                    password=password
+        )
 
-    def do_login(self, user, password):
-        in_data = ""
-        url = ""
-        
-        in_data = """{{ "username":"{username}", "password":"{password}" }}""".format(username=user, password=password)
-        
         url = self.base_url + "/oasis/login"
-        
-        
         response = self.do_request(url, in_data)
         json_response = json.loads(response.content)
         
-        #This is used for debugging HTNML outputs.
-        #display(HTML(strResponse.content))
-        
-        #InitScriptEngine
-        #Set JsonObject = DecodeJsonString(CStr(strResponse))
-        #value = GetProperty(JsonObject, "success")
-        
-        
-        value = json_response["success"]
-        if value == False:
+        if json_response["success"] == False:
             print("Invalid user id or password")
         else:
-            #writetoconsole objHTTP.GetResponseHeader("Set-Cookie")
             self.cookies = dict(sessionid=response.cookies['sessionid'])
             print("You are logged into Mid-tier")
+
+    def upload_file(self, local_absolute_filepath, upload_filename):
+        url = self.base_url + "/oasis/doTaskUploadFileHelper/"
+
+        with open(local_absolute_filepath) as f:
+            in_file = f
+            response = self.do_request(
+                url,
+                #TODO: Get the upload_filename to actually work.
+                in_file_dict={upload_filename:in_file}
+            )
+            return response
