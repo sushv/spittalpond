9eb42aea8e942dea68231c4f2b43988541a1b338
Beckett Simmons <beckettsimmons@hotmail.com>
2015-04-29 12:17:17 +0100

    Reverting spittalpond/spittalpond.py to 8eec33.
    For whatever reason we overwrote this when merging config.

--- spittalpond/spittalpond.py
+++ spittalpond/spittalpond.py
@@ -6,24 +6,32 @@ import logging
 class SpittalPond():
     """ Python interface to the Oasis Django API.
 
-    This class provides an easy to use front-end interface
-    to the Oasis mid-tier Django API.
+    This class provides an easy to use front-end interface to the Oasis
+    mid-tier Django API.
     """
 
-    # TODO: Fix the the default log location below to work on Windows.
-    def __init__(self, base_url, pub_user,
-                 log_file='/var/tmp/logger.log', log_level=logging.INFO):
-        # Initiating our sub classes
+    def __init__(self, base_url, user,
+                 log_file=None, log_level=logging.INFO):
+        """ Initiate with server URL and user.
+
+        Args:
+            base_url (str): The URL of the Django server. Be sure to prepend
+                the protocol (i.e. http://) and append the port (i.e. :8000).
+            user (str): Username to use on the server.
+        """
+
         logger = logging.getLogger('spittalpond')
-        logger.propagate = False
-        handler = logging.FileHandler(log_file)
-        formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(log_level)
+        if log_file == None:
+            logger.disabled = True
+        else:
+            logger.propagate = False
+            handler = logging.FileHandler(log_file)
+            formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+            logger.setLevel(log_level)
 
         logger.info('Initating new spittalpond instance.')
-
-        self.model = SpittalModel(base_url, pub_user)
-        self.exposure = SpittalExposure(base_url, pub_user)
-        self.run = SpittalRun(base_url, pub_user)
+        self.model = SpittalModel(base_url, user)
+        self.exposure = SpittalExposure(base_url, user)
+        self.run = SpittalRun(base_url, user)
